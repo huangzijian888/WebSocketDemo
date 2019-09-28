a8c9c7eb2a55facf5d5244c51c8d450cb0998f1d
huangzijian888 <huangzijian888@gmail.com>
2019-09-29 05:10:17 +0800

    完成基本功能

---
+++ src/main/java/cn/huangzijian888/websocket/WebsocketApplication.java
@@ -0,0 +1,16 @@
+package cn.huangzijian888.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huangzijian888
+ */
+@SpringBootApplication
+public class WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
+
+}
